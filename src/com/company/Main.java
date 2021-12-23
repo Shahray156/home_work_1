31d18471e10628256e7ffd869a416ac4243d96da
Shahray156 <shahray1561@gmail.com>
2021-12-23 22:08:35 +0300

    Второй коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte apples = 95;
+        System.out.println("Яблок " + apples + " кг");
+
+        short bananas = 212;
+        System.out.println("Бананов " + bananas + " кг");
+
+        int age = 45;
+        System.out.println(age);
+
+        long a = 200000L;
+        System.out.println(a);
+
+        float f = 156.7f;
+        System.out.println(f);
+
+        double d = 566.1;
+        System.out.println(d);
+
     }
 }
